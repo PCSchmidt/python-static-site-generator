905d0c0c5ddb6f12c074307991462276300b714a
Chris Schmidt <p.christopher.schmidt@gmail.com>
2023-04-18 19:39:36 -0400

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -3,27 +3,30 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, cls, string):
-        _, fm, content = __regex.split(string, 2)
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
         load(fm, Loader=FullLoader)
-        cls(metadata, content)
+        Return cls(metadata, content)
 
-    def Content(self, metadata, content):
+    def __init__(self, metadata, content):
         self.data = metadata
         self.data["content"] = content
-        @property
-        def body(self):
-            return self.data["content"] if "content" in self.data else None
-        @property
-        def type(self):
-            return self.data["type"] if "type" in self.data else None
+
+    @property
+    def body(self):
+        return self.data["content"] 
+    
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
         
     @type.setter
-    def type(self, value):
-        self.data["type"] = value
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
@@ -33,8 +36,7 @@ class Content(Mapping):
     
     def __repr__(self):
         data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
         return str(data)
-    
-    for key, value in self.data.items():
-        if key != "content":
-            data[key] = value
